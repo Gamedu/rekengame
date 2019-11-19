59c2e73e91b4407f0f4a8fa5d7c2fb7ca25366c4
Brian <Brian@BRIAN>
2019-11-19 15:22:08 +0100

    Aangemaakt door Bri Bri

--- rekenen/rekenen/Form1.Designer.cs
+++ rekenen/rekenen/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.tbStudentNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Leerling nummer:";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.Location = new System.Drawing.Point(68, 69);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(100, 100);
+            this.btnLogIn.TabIndex = 1;
+            this.btnLogIn.Text = "Log in";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.BtnLogIn_Click);
+            // 
+            // tbStudentNumber
+            // 
+            this.tbStudentNumber.Location = new System.Drawing.Point(68, 43);
+            this.tbStudentNumber.Name = "tbStudentNumber";
+            this.tbStudentNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbStudentNumber.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(240, 195);
+            this.Controls.Add(this.tbStudentNumber);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.TextBox tbStudentNumber;
     }
 }
 
--- rekenen/rekenen/Form1.cs
+++ rekenen/rekenen/Form1.cs
@@ -16,5 +16,19 @@ namespace rekenen
         {
             InitializeComponent();
         }
+
+        private void BtnLogIn_Click(object sender, EventArgs e)
+        {
+            // Check of leerling nummer voorkomt in database.
+            // Nee -> "leerling nummer klopt niet"
+            //Ja -> afhankelijk van de klas waar leerling nummer voorkomt, ga naar reken app. (Form 2 t/m 5)
+
+            // - Groep 3: +/- t/m 20
+            //- Groep 4: +/ -t / m 100, x5
+            // - Groep 5: +/ -t / m 1000
+            // - Groep 6: +/ -t / m 100.000, breuken, x10, delen
+            // - Groep 7 + 8: +/ -t / m 1.000.000, breuken, schaalsommen, 
+
+        }
     }
 }
