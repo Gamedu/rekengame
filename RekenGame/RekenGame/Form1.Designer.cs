f073b537354a60daa6f83766e884cf1ac0ddd62e
JeroneHeuveling <57947315+JeroneHeuveling@users.noreply.github.com>
2019-11-19 14:28:33 +0100

    ppbig

--- RekenGame/RekenGame/Form1.Designer.cs
+++ RekenGame/RekenGame/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(697, 399);
+            this.button1.Location = new System.Drawing.Point(275, 159);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(220, 156);
             this.button1.TabIndex = 0;
             this.button1.Text = "Henk";
             this.button1.UseVisualStyleBackColor = true;
