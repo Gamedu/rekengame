3f9351b58a1b85dae72589a0ce5b8068c2b6e03c
Brian <Brian@BRIAN>
2019-11-20 11:48:53 +0100

    Een hoop shizzle

--- rekenen/rekenen/Form1.Designer.cs
+++ rekenen/rekenen/Form1.Designer.cs
@@ -28,59 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rbPlus = new System.Windows.Forms.RadioButton();
+            this.rbMinus = new System.Windows.Forms.RadioButton();
+            this.rbMultiply = new System.Windows.Forms.RadioButton();
+            this.rbDivide = new System.Windows.Forms.RadioButton();
+            this.rbTest = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblEquation = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnLogIn = new System.Windows.Forms.Button();
-            this.tbStudentNumber = new System.Windows.Forms.TextBox();
+            this.tbAnswer = new System.Windows.Forms.TextBox();
+            this.tbScore = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnNewEquation = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(112, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Go";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // rbPlus
+            // 
+            this.rbPlus.AutoSize = true;
+            this.rbPlus.Location = new System.Drawing.Point(6, 18);
+            this.rbPlus.Name = "rbPlus";
+            this.rbPlus.Size = new System.Drawing.Size(31, 17);
+            this.rbPlus.TabIndex = 2;
+            this.rbPlus.TabStop = true;
+            this.rbPlus.Text = "+";
+            this.rbPlus.UseVisualStyleBackColor = true;
+            // 
+            // rbMinus
+            // 
+            this.rbMinus.AutoSize = true;
+            this.rbMinus.Location = new System.Drawing.Point(6, 41);
+            this.rbMinus.Name = "rbMinus";
+            this.rbMinus.Size = new System.Drawing.Size(28, 17);
+            this.rbMinus.TabIndex = 3;
+            this.rbMinus.TabStop = true;
+            this.rbMinus.Text = "-";
+            this.rbMinus.UseVisualStyleBackColor = true;
+            // 
+            // rbMultiply
+            // 
+            this.rbMultiply.AutoSize = true;
+            this.rbMultiply.Location = new System.Drawing.Point(6, 64);
+            this.rbMultiply.Name = "rbMultiply";
+            this.rbMultiply.Size = new System.Drawing.Size(29, 17);
+            this.rbMultiply.TabIndex = 4;
+            this.rbMultiply.TabStop = true;
+            this.rbMultiply.Text = "*";
+            this.rbMultiply.UseVisualStyleBackColor = true;
+            // 
+            // rbDivide
+            // 
+            this.rbDivide.AutoSize = true;
+            this.rbDivide.Location = new System.Drawing.Point(6, 87);
+            this.rbDivide.Name = "rbDivide";
+            this.rbDivide.Size = new System.Drawing.Size(30, 17);
+            this.rbDivide.TabIndex = 5;
+            this.rbDivide.TabStop = true;
+            this.rbDivide.Text = "/";
+            this.rbDivide.UseVisualStyleBackColor = true;
+            // 
+            // rbTest
+            // 
+            this.rbTest.AutoSize = true;
+            this.rbTest.Location = new System.Drawing.Point(6, 110);
+            this.rbTest.Name = "rbTest";
+            this.rbTest.Size = new System.Drawing.Size(52, 17);
+            this.rbTest.TabIndex = 6;
+            this.rbTest.TabStop = true;
+            this.rbTest.Text = "Toets";
+            this.rbTest.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbMinus);
+            this.groupBox1.Controls.Add(this.rbPlus);
+            this.groupBox1.Controls.Add(this.rbTest);
+            this.groupBox1.Controls.Add(this.rbMultiply);
+            this.groupBox1.Controls.Add(this.rbDivide);
+            this.groupBox1.Location = new System.Drawing.Point(12, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(64, 129);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Instellingen";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(449, 49);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Klas";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnNewEquation);
+            this.groupBox3.Controls.Add(this.tbAnswer);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.lblEquation);
+            this.groupBox3.Location = new System.Drawing.Point(82, 67);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(173, 129);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sommen";
+            // 
+            // lblEquation
+            // 
+            this.lblEquation.AutoSize = true;
+            this.lblEquation.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquation.Location = new System.Drawing.Point(6, 22);
+            this.lblEquation.Name = "lblEquation";
+            this.lblEquation.Size = new System.Drawing.Size(147, 39);
+            this.lblEquation.TabIndex = 0;
+            this.lblEquation.Text = "vb: 8 + 7";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(6, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Leerling nummer:";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Antwoord:";
             // 
-            // btnLogIn
+            // tbAnswer
             // 
-            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogIn.Location = new System.Drawing.Point(68, 69);
-            this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(100, 100);
-            this.btnLogIn.TabIndex = 1;
-            this.btnLogIn.Text = "Log in";
-            this.btnLogIn.UseVisualStyleBackColor = true;
-            this.btnLogIn.Click += new System.EventHandler(this.BtnLogIn_Click);
+            this.tbAnswer.Location = new System.Drawing.Point(67, 64);
+            this.tbAnswer.Name = "tbAnswer";
+            this.tbAnswer.Size = new System.Drawing.Size(100, 20);
+            this.tbAnswer.TabIndex = 2;
             // 
-            // tbStudentNumber
+            // tbScore
             // 
-            this.tbStudentNumber.Location = new System.Drawing.Point(68, 43);
-            this.tbStudentNumber.Name = "tbStudentNumber";
-            this.tbStudentNumber.Size = new System.Drawing.Size(100, 20);
-            this.tbStudentNumber.TabIndex = 2;
+            this.tbScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 65F);
+            this.tbScore.Location = new System.Drawing.Point(6, 19);
+            this.tbScore.Multiline = true;
+            this.tbScore.Name = "tbScore";
+            this.tbScore.Size = new System.Drawing.Size(188, 98);
+            this.tbScore.TabIndex = 3;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tbScore);
+            this.groupBox4.Location = new System.Drawing.Point(261, 71);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 123);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Score";
+            // 
+            // btnNewEquation
+            // 
+            this.btnNewEquation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewEquation.Location = new System.Drawing.Point(13, 95);
+            this.btnNewEquation.Name = "btnNewEquation";
+            this.btnNewEquation.Size = new System.Drawing.Size(154, 23);
+            this.btnNewEquation.TabIndex = 3;
+            this.btnNewEquation.Text = "Nieuwe Som";
+            this.btnNewEquation.UseVisualStyleBackColor = true;
+            this.btnNewEquation.Click += new System.EventHandler(this.BtnNewEquation_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(240, 195);
-            this.Controls.Add(this.tbStudentNumber);
-            this.Controls.Add(this.btnLogIn);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(468, 197);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RadioButton rbPlus;
+        private System.Windows.Forms.RadioButton rbMinus;
+        private System.Windows.Forms.RadioButton rbMultiply;
+        private System.Windows.Forms.RadioButton rbDivide;
+        private System.Windows.Forms.RadioButton rbTest;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tbAnswer;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnLogIn;
-        private System.Windows.Forms.TextBox tbStudentNumber;
+        private System.Windows.Forms.Label lblEquation;
+        private System.Windows.Forms.TextBox tbScore;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnNewEquation;
     }
 }
 
--- rekenen/rekenen/Form1.cs
+++ rekenen/rekenen/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace rekenen
@@ -17,18 +10,57 @@ namespace rekenen
             InitializeComponent();
         }
 
-        private void BtnLogIn_Click(object sender, EventArgs e)
+        private void BtnStart_Click(object sender, EventArgs e)
         {
-            // Check of leerling nummer voorkomt in database.
-            // Nee -> "leerling nummer klopt niet"
-            //Ja -> afhankelijk van de klas waar leerling nummer voorkomt, ga naar reken app. (Form 2 t/m 5)
+            Test(Int32.Parse(textBox1.Text));
+        }
 
-            // - Groep 3: +/- t/m 20
-            //- Groep 4: +/ -t / m 100, x5
-            // - Groep 5: +/ -t / m 1000
-            // - Groep 6: +/ -t / m 100.000, breuken, x10, delen
-            // - Groep 7 + 8: +/ -t / m 1.000.000, breuken, schaalsommen, 
+        public void Test(int groep)
+        {
+            switch (groep)
+            {
+                case 3:
+                    rbMultiply.Hide();
+                    rbDivide.Hide();
+                    break;
+                case 4:
+                    rbDivide.Hide();
+                    break;
+                case 5:
 
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+                case 8:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        private void BtnNewEquation_Click(object sender, EventArgs e)
+        {
+            Som();
+        }
+
+        int a;
+        int b;
+
+        public void Som()
+        {
+            Random rnd = new Random();
+            
+            a = rnd.Next(50);
+            b = rnd.Next(50);
+
+            if (rbPlus.Checked)
+            {
+                lblEquation.Text = 
+            }
         }
     }
 }
