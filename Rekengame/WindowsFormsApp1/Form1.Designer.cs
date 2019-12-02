074ee7033360123e6fb0ba006a3f211869b2e59e
Brian <Brian@BRIAN>
2019-12-02 11:36:50 +0100

    Pauze

--- Rekengame/WindowsFormsApp1/Form1.Designer.cs
+++ Rekengame/WindowsFormsApp1/Form1.Designer.cs
@@ -34,13 +34,15 @@
             this.rbMinus = new System.Windows.Forms.RadioButton();
             this.rbMultiply = new System.Windows.Forms.RadioButton();
             this.rbDivide = new System.Windows.Forms.RadioButton();
-            this.RBGroup = new System.Windows.Forms.GroupBox();
             this.lblSum = new System.Windows.Forms.Label();
-            this.lblScore = new System.Windows.Forms.Label();
             this.GameCountDown = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.tbAnswer = new System.Windows.Forms.TextBox();
-            this.RBGroup.SuspendLayout();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlButtons.SuspendLayout();
             this.SuspendLayout();
             // 
             // rbPlus
@@ -49,12 +51,15 @@
             this.rbPlus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbPlus.BackgroundImage")));
             this.rbPlus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.rbPlus.FlatAppearance.BorderSize = 0;
-            this.rbPlus.Location = new System.Drawing.Point(6, 19);
+            this.rbPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPlus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.rbPlus.Location = new System.Drawing.Point(4, 3);
             this.rbPlus.Name = "rbPlus";
             this.rbPlus.Size = new System.Drawing.Size(75, 75);
             this.rbPlus.TabIndex = 0;
             this.rbPlus.TabStop = true;
             this.rbPlus.Tag = "Plus";
+            this.rbPlus.Text = "+";
             this.rbPlus.UseVisualStyleBackColor = true;
             this.rbPlus.Click += new System.EventHandler(this.RadioBtn_Click);
             // 
@@ -64,12 +69,14 @@
             this.rbMinus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbMinus.BackgroundImage")));
             this.rbMinus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.rbMinus.FlatAppearance.BorderSize = 0;
-            this.rbMinus.Location = new System.Drawing.Point(6, 100);
+            this.rbMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMinus.Location = new System.Drawing.Point(3, 84);
             this.rbMinus.Name = "rbMinus";
             this.rbMinus.Size = new System.Drawing.Size(75, 75);
             this.rbMinus.TabIndex = 1;
             this.rbMinus.TabStop = true;
             this.rbMinus.Tag = "Minus";
+            this.rbMinus.Text = "-";
             this.rbMinus.UseVisualStyleBackColor = true;
             this.rbMinus.Click += new System.EventHandler(this.RadioBtn_Click);
             // 
@@ -79,12 +86,14 @@
             this.rbMultiply.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbMultiply.BackgroundImage")));
             this.rbMultiply.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.rbMultiply.FlatAppearance.BorderSize = 0;
-            this.rbMultiply.Location = new System.Drawing.Point(6, 181);
+            this.rbMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMultiply.Location = new System.Drawing.Point(3, 165);
             this.rbMultiply.Name = "rbMultiply";
             this.rbMultiply.Size = new System.Drawing.Size(75, 75);
             this.rbMultiply.TabIndex = 2;
             this.rbMultiply.TabStop = true;
             this.rbMultiply.Tag = "Multiply";
+            this.rbMultiply.Text = "*";
             this.rbMultiply.UseVisualStyleBackColor = true;
             this.rbMultiply.Click += new System.EventHandler(this.RadioBtn_Click);
             // 
@@ -94,65 +103,88 @@
             this.rbDivide.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbDivide.BackgroundImage")));
             this.rbDivide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.rbDivide.FlatAppearance.BorderSize = 0;
-            this.rbDivide.Location = new System.Drawing.Point(6, 262);
+            this.rbDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDivide.Location = new System.Drawing.Point(3, 246);
             this.rbDivide.Name = "rbDivide";
             this.rbDivide.Size = new System.Drawing.Size(75, 75);
             this.rbDivide.TabIndex = 3;
             this.rbDivide.TabStop = true;
             this.rbDivide.Tag = "Divide";
+            this.rbDivide.Text = "/";
             this.rbDivide.UseVisualStyleBackColor = true;
             this.rbDivide.Click += new System.EventHandler(this.RadioBtn_Click);
-            // 
-            // RBGroup
-            // 
-            this.RBGroup.Controls.Add(this.rbPlus);
-            this.RBGroup.Controls.Add(this.rbDivide);
-            this.RBGroup.Controls.Add(this.rbMinus);
-            this.RBGroup.Controls.Add(this.rbMultiply);
-            this.RBGroup.Location = new System.Drawing.Point(12, 31);
-            this.RBGroup.Name = "RBGroup";
-            this.RBGroup.Size = new System.Drawing.Size(91, 341);
-            this.RBGroup.TabIndex = 4;
-            this.RBGroup.TabStop = false;
-            this.RBGroup.Text = "groupBox1";
             // 
             // lblSum
             // 
             this.lblSum.AutoSize = true;
-            this.lblSum.Location = new System.Drawing.Point(364, 121);
+            this.lblSum.Location = new System.Drawing.Point(127, 23);
             this.lblSum.Name = "lblSum";
-            this.lblSum.Size = new System.Drawing.Size(35, 13);
+            this.lblSum.Size = new System.Drawing.Size(28, 13);
             this.lblSum.TabIndex = 5;
-            this.lblSum.Text = "label1";
+            this.lblSum.Text = "Som";
+            // 
+            // tbAnswer
+            // 
+            this.tbAnswer.Location = new System.Drawing.Point(100, 61);
+            this.tbAnswer.Name = "tbAnswer";
+            this.tbAnswer.Size = new System.Drawing.Size(100, 20);
+            this.tbAnswer.TabIndex = 9;
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Controls.Add(this.rbPlus);
+            this.pnlButtons.Controls.Add(this.rbDivide);
+            this.pnlButtons.Controls.Add(this.rbMultiply);
+            this.pnlButtons.Controls.Add(this.rbMinus);
+            this.pnlButtons.Location = new System.Drawing.Point(12, 12);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(82, 325);
+            this.pnlButtons.TabIndex = 10;
             // 
             // lblScore
             // 
             this.lblScore.AutoSize = true;
-            this.lblScore.Location = new System.Drawing.Point(664, 372);
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(337, 23);
             this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(35, 13);
-            this.lblScore.TabIndex = 8;
-            this.lblScore.Text = "label2";
+            this.lblScore.Size = new System.Drawing.Size(0, 31);
+            this.lblScore.TabIndex = 4;
             // 
-            // tbAnswer
+            // btnStart
             // 
-            this.tbAnswer.Location = new System.Drawing.Point(337, 159);
-            this.tbAnswer.Name = "tbAnswer";
-            this.tbAnswer.Size = new System.Drawing.Size(100, 20);
-            this.tbAnswer.TabIndex = 9;
+            this.btnStart.Location = new System.Drawing.Point(113, 87);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 31);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Score:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tbAnswer);
+            this.ClientSize = new System.Drawing.Size(852, 508);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.tbAnswer);
             this.Controls.Add(this.lblSum);
-            this.Controls.Add(this.RBGroup);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.RBGroup.ResumeLayout(false);
+            this.pnlButtons.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,12 +196,14 @@
         private System.Windows.Forms.RadioButton rbMinus;
         private System.Windows.Forms.RadioButton rbMultiply;
         private System.Windows.Forms.RadioButton rbDivide;
-        private System.Windows.Forms.GroupBox RBGroup;
         private System.Windows.Forms.Label lblSum;
-        private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.Timer GameCountDown;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.TextBox tbAnswer;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Rekengame/WindowsFormsApp1/Form1.cs
+++ Rekengame/WindowsFormsApp1/Form1.cs
@@ -7,20 +7,61 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        //Global variables\\
+        private readonly Random rnd = new Random();
+        private int randomNumberOne;
+        private int randomNumberTwo;
+        private int score;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //Click event for all the radio buttons in the panel\\
         private void RadioBtn_Click(object sender, EventArgs e)
         {
-            var radioButtonSender = (RadioButton)sender;
+            btnStart.Text = "Start";
 
-            foreach (var rb in this.RBGroup.Controls.OfType<RadioButton>())
+            var radioButtonSender = (RadioButton)sender;
+            foreach (var rb in this.pnlButtons.Controls.OfType<RadioButton>())
             {
                 var btnColour = rb == radioButtonSender ? "Green" : "Red";
                 rb.BackgroundImage = Image.FromFile($@"Images\Button_{btnColour}_{(string)rb.Tag}.jpg");
+                GenerateNumbers(1, 50, 1, 50 );
+                lblSum.Text = "{randomNumberOne}" + rb.Text + "{randomNumberTwo}";
             }
+        }
+
+        //Click event for the start button\\
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "Controleer")
+            {
+                if (Convert.ToInt32(tbAnswer.Text) == randomNumberOne + randomNumberTwo)
+                {
+                    score++;
+                    lblScore.Text = Convert.ToString(score);
+                }
+                else
+                {
+                    score--;
+                    lblScore.Text = Convert.ToString(score);
+                }
+            }
+
+            tbAnswer.Clear();
+            btnStart.Text = "Controleer";
+
+            GenerateNumbers(1, 10, 1, 10);
+            lblSum.Text = $"Wat is {randomNumberOne} + {randomNumberTwo}";
+        }
+
+        //Method for creating two random numbers that are temporarily stored as two variables\\
+        private void GenerateNumbers(int minValueOne, int maxValueOne, int minValueTwo, int maxValueTwo)
+        {
+            randomNumberOne = rnd.Next(minValueOne, maxValueOne);
+            randomNumberTwo = rnd.Next(minValueTwo, maxValueTwo);
         }
     }
 }
