05a9107a9ac76be26bb356bcbd3ff4a88102bbdc
Brian <Brian@BRIAN>
2019-12-02 10:20:57 +0100

    Alles ist kaput

---
+++ Rekengame/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rbPlus = new System.Windows.Forms.RadioButton();
+            this.rbMinus = new System.Windows.Forms.RadioButton();
+            this.rbMultiply = new System.Windows.Forms.RadioButton();
+            this.rbDivide = new System.Windows.Forms.RadioButton();
+            this.GameCountDown = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbPlus
+            // 
+            this.rbPlus.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rbPlus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbPlus.BackgroundImage")));
+            this.rbPlus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbPlus.FlatAppearance.BorderSize = 0;
+            this.rbPlus.Location = new System.Drawing.Point(3, 5);
+            this.rbPlus.Name = "rbPlus";
+            this.rbPlus.Size = new System.Drawing.Size(75, 75);
+            this.rbPlus.TabIndex = 0;
+            this.rbPlus.TabStop = true;
+            this.rbPlus.Tag = "Plus";
+            this.rbPlus.UseVisualStyleBackColor = true;
+            this.rbPlus.Click += new System.EventHandler(this.RadioBtn_Click);
+            // 
+            // rbMinus
+            // 
+            this.rbMinus.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rbMinus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbMinus.BackgroundImage")));
+            this.rbMinus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbMinus.FlatAppearance.BorderSize = 0;
+            this.rbMinus.Location = new System.Drawing.Point(3, 86);
+            this.rbMinus.Name = "rbMinus";
+            this.rbMinus.Size = new System.Drawing.Size(75, 75);
+            this.rbMinus.TabIndex = 1;
+            this.rbMinus.TabStop = true;
+            this.rbMinus.Tag = "Minus";
+            this.rbMinus.UseVisualStyleBackColor = true;
+            this.rbMinus.Click += new System.EventHandler(this.RadioBtn_Click);
+            // 
+            // rbMultiply
+            // 
+            this.rbMultiply.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rbMultiply.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbMultiply.BackgroundImage")));
+            this.rbMultiply.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbMultiply.FlatAppearance.BorderSize = 0;
+            this.rbMultiply.Location = new System.Drawing.Point(3, 167);
+            this.rbMultiply.Name = "rbMultiply";
+            this.rbMultiply.Size = new System.Drawing.Size(75, 75);
+            this.rbMultiply.TabIndex = 2;
+            this.rbMultiply.TabStop = true;
+            this.rbMultiply.Tag = "Multiply";
+            this.rbMultiply.UseVisualStyleBackColor = true;
+            this.rbMultiply.Click += new System.EventHandler(this.RadioBtn_Click);
+            // 
+            // rbDivide
+            // 
+            this.rbDivide.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rbDivide.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbDivide.BackgroundImage")));
+            this.rbDivide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rbDivide.FlatAppearance.BorderSize = 0;
+            this.rbDivide.Location = new System.Drawing.Point(3, 248);
+            this.rbDivide.Name = "rbDivide";
+            this.rbDivide.Size = new System.Drawing.Size(75, 75);
+            this.rbDivide.TabIndex = 3;
+            this.rbDivide.TabStop = true;
+            this.rbDivide.Tag = "Divide";
+            this.rbDivide.UseVisualStyleBackColor = true;
+            this.rbDivide.Click += new System.EventHandler(this.RadioBtn_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(227, 152);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "button1";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.BackColor = System.Drawing.Color.Transparent;
+            this.lblTest.Location = new System.Drawing.Point(248, 98);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(35, 13);
+            this.lblTest.TabIndex = 6;
+            this.lblTest.Text = "label1";
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Controls.Add(this.rbPlus);
+            this.pnlButtons.Controls.Add(this.rbDivide);
+            this.pnlButtons.Controls.Add(this.rbMultiply);
+            this.pnlButtons.Controls.Add(this.rbMinus);
+            this.pnlButtons.Location = new System.Drawing.Point(12, 12);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(83, 328);
+            this.pnlButtons.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 369);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(318, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.btnTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbPlus;
+        private System.Windows.Forms.RadioButton rbMinus;
+        private System.Windows.Forms.RadioButton rbMultiply;
+        private System.Windows.Forms.RadioButton rbDivide;
+        private System.Windows.Forms.Timer GameCountDown;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rekengame/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile(@"Images\Jungle_Kids.jpg");
+        }
+
+        private void RadioBtn_Click(object sender, EventArgs e)
+        {
+            var radioButtonSender = (RadioButton)sender;
+
+            foreach (var rb in this.pnlButtons.Controls.OfType<RadioButton>())
+            {
+                var btnColour = rb == radioButtonSender ? "Green" : "Red";
+                rb.BackgroundImage = Image.FromFile($@"Images\Button_{btnColour}_{(string)rb.Tag}.jpg");
+            }
+        }
+    }
+}
